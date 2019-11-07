159547e517372746ccca18b8195aeed1b0c1b51b
Cynderina <40639249+Cynderina@users.noreply.github.com>
2019-11-07 15:25:20 +0200

    Update Document.cs

--- Document.cs
+++ Document.cs
@@ -14,23 +14,17 @@ namespace Inheritance
         private string _supplier;
         private double _total;
 
-        //Document ID made to check that program works. There are different lists for orders and invoices in main program for now and therefore list index could also work as ID.
-        static int _amountOfDocuments = 1;
-        private int _id;
+       
 
         public Document(string orderNumber, string supplier, double total)
         {
             _orderNumber = orderNumber;
             _supplier = supplier;
             _total = total;
-            _id = _amountOfDocuments;
-            _amountOfDocuments++;
+           
         }
 
-        public int GetId()
-        {
-            return _id;
-        }
+      
 
         public string GetOrderNumber()
         {
